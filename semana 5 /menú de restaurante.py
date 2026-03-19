89ebc73ab5550a42a0ffbf88e401cf14d78b4688
madrigalangel148-dot <madrigalangel148@gmail.com>
2026-03-18 22:38:17 -0500

    Create menú de restaurante.py

---
+++ semana 5 /menú de restaurante.py
@@ -0,0 +1,9 @@
+opcion=int(input("elige una opcion del menu (1,2 o 3):"))
+if opcion == 1:
+    print("elegiste: bandeja paisa - $18.000")
+elif opcion == 2:
+    print("elegiste: ajiaco - $15.000")
+elif opcion == 3:
+    print("elegiste: sancocho - $12.000")
+else:
+    print("opcion no valida, intenta de nuevo")
