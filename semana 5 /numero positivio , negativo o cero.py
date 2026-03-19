e0f543d127c9578264ff46eed155124809d94d4c
madrigalangel148-dot <madrigalangel148@gmail.com>
2026-03-18 21:38:58 -0500

    Create numero positivio , negativo o cero.py

---
+++ semana 5 /numero positivio , negativo o cero.py
@@ -0,0 +1,7 @@
+numero=int(input("ingrese un numero entero:"))
+if numero >0:
+    print("el numero es positivo")
+elif numero <0:
+    print("el numero es negativo")
+else:
+    print("el numero es cero")
