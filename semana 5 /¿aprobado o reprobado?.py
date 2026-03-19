5298fc580cb30bc410c29df75f25cf3fbfd4b67c
madrigalangel148-dot <madrigalangel148@gmail.com>
2026-03-18 21:47:35 -0500

    Create ¿aprobado o reprobado?.py

---
+++ semana 5 /¿aprobado o reprobado?.py
@@ -0,0 +1,5 @@
+nota=float(input("ingrese la nota final del estudiante:"))
+if nota >=3.0:
+    print("aprobaste la materia. ¡ felicidades !")
+else:
+    print("reprobaste la materia. debes presentar habilitacion")
