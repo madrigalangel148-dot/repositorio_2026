290cd780c4ea6ff88969378a1acd35f6edb360da
madrigalangel148-dot <madrigalangel148@gmail.com>
2026-03-27 21:28:38 -0500

    Add user authentication script

---
+++ semana 7/acceso al sistema.py
@@ -0,0 +1,7 @@
+nombre=input("dame tu usuario:")
+contraseña=input("dame la contraseña:")
+
+if nombre =="admin"and "contraseña" =="1234":
+    print("contraseña correcta")
+else:
+    print("contraseña incorrecta no puedes ingresar")
