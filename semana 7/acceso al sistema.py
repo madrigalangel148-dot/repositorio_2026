68650e7adc2096fe7d82455e89b9d78ac2c88126
madrigalangel148-dot <madrigalangel148@gmail.com>
2026-09-15 21:26:18 -0500

    Delete semana 7 directory

--- semana 7/acceso al sistema.py
+++
@@ -1,7 +0,0 @@
-nombre=input("dame tu usuario:")
-contraseña=input("dame la contraseña:")
-
-if nombre =="admin"and "contraseña" =="1234":
-    print("contraseña correcta")
-else:
-    print("contraseña incorrecta no puedes ingresar")
--- semana 7/permiso para conducir.py
+++
@@ -1,6 +0,0 @@
-edad=int(input("ingrese su edad:"))
-licencia=input("¿tiene licencia de conducir? (si/no):")
-if edad >= 18 and licencia=="si":
-    print("¡puedes conducir con cuidado")
-else:
-    print("no puedes conducir, necesitas tener al menos 18 años y tu licencia")
